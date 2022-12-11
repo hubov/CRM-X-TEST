eba9745db444e2393ea206beffd4e6a85d645449
Hubert Owerczuk <hub.ower@gmail.com>
2022-12-11 20:20:28 +0100

    Add import data to Importer index view

--- resources/views/Importer/index.blade.php
+++ resources/views/Importer/index.blade.php
@@ -10,9 +10,11 @@
         @if (count($list) == 0)
             No uploads.
         @else
+            <table><thead><th>Run at</th><th>Entries processed</th><th>Entries created</th></thead>
             @foreach($list as $upload)
-                {{ $upload->run_at }}
+                    <td><a href="{{ Storage::url('reports/'.$upload->id.'.csv') }}">{{ $upload->run_at }}</a></td><td>{{ $upload->entries_processed }}</td><td>{{ $upload->entries_created }}</td></tr>
             @endforeach
+            </table>
         @endif
     </body>
 </html>
