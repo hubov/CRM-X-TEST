a3843ed52cac6684452e18a7148ad9b537085843
Hubert Owerczuk <hub.ower@gmail.com>
2022-12-11 18:33:54 +0100

    Add default values to Importer model

--- app/Modules/Importer/Models/Importer.php
+++ app/Modules/Importer/Models/Importer.php
@@ -12,8 +12,27 @@ class Importer extends LogModel
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'modified_at';
 
-    protected $fillable = ['type', 'run_at', 'entries_processed', 'entries_created'];
+    protected $fillable = [
+        'type',
+        'run_at',
+        'entries_processed',
+        'entries_created'
+    ];
 
+    public function __construct(array $attributes = [])
+    {
+        if (!isset($attributes['run_at'])) {
+            $attributes['run_at'] = (new \DateTime('now'))->format('Y-m-d H:i:s');
+        }
+        if (!isset($attributes['entries_processed'])) {
+            $attributes['entries_processed'] = 0;
+        }
+        if (!isset($attributes['entries_created'])) {
+            $attributes['entries_created'] = 0;
+        }
+
+        parent::__construct($attributes);
+    }
     // relationships
 
     // scopes
